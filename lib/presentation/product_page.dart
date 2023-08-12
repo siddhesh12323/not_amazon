f99fbe9dbd8499cbac21b77dea8557b7e13224af
Siddhesh Kumbhar <purnakumbhar1234@gmail.com>
2023-08-12 17:46:54 +0530

    add to cart and buy now

--- lib/presentation/product_page.dart
+++ lib/presentation/product_page.dart
@@ -47,98 +47,152 @@ class _ProductPageState extends State<ProductPage> {
           elevation: 0,
           foregroundColor: const Color.fromARGB(255, 98, 95, 95),
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Stack(
-              alignment: Alignment.bottomCenter,
-              children: [
-                SizedBox(
-                  height: 300,
-                  width: double.infinity,
-                  child: PageView.builder(
-                      onPageChanged: (value) {
-                        setState(() {
-                          currentIndex = value % product!.images.length;
-                        });
-                      },
-                      itemCount: product!.images.length,
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20),
-                          child: SizedBox(
-                            height: 300,
-                            width: double.infinity,
-                            child: Image.asset(
-                              product!.images[index % product.images.length],
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        );
-                      }),
-                ),
-                Row(
-                  children: [
-                    IconButton(onPressed: () {}, icon: Icon(Icons.share)),
-                    const Spacer(),
-                    IconButton(
-                        onPressed: () {}, icon: Icon(Icons.favorite_outline)),
-                  ],
-                )
-              ],
-            ),
-            getSizedBox(10, 0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                for (var i = 0; i < product.images.length; i++)
-                  buildIndicator(currentIndex == i)
-              ],
-            ),
-            getSizedBox(10, 0),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Row(
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              //! PRODUCT IMAGES
+              Stack(
+                alignment: Alignment.bottomCenter,
                 children: [
-                  InkWell(
-                    onTap: () => _launchURL(product!.url),
-                    child: const Text(
-                      'Visit the company store',
-                      style: TextStyle(color: Colors.blue, fontSize: 16),
-                    ),
+                  SizedBox(
+                    height: 300,
+                    width: double.infinity,
+                    child: PageView.builder(
+                        onPageChanged: (value) {
+                          setState(() {
+                            currentIndex = value % product!.images.length;
+                          });
+                        },
+                        itemCount: product!.images.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            child: SizedBox(
+                              height: 300,
+                              width: double.infinity,
+                              child: Image.asset(
+                                product!.images[index % product.images.length],
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          );
+                        }),
                   ),
-                  const Spacer(),
-                  Text(
-                    '${product.rating.toString()}/5.0',
-                    style: TextStyle(fontSize: 16),
+                  Row(
+                    children: [
+                      IconButton(
+                          onPressed: () {}, icon: const Icon(Icons.share)),
+                      const Spacer(),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.favorite_outline)),
+                    ],
                   )
                 ],
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 5),
-              child: Text(
-                product.name,
-                style:
-                    const TextStyle(fontSize: 23, fontWeight: FontWeight.w500),
+              getSizedBox(10, 0),
+              //! NUMBER OF IMAGES INDICATOR
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  for (var i = 0; i < product.images.length; i++)
+                    buildIndicator(currentIndex == i)
+                ],
               ),
-            ),
-            Padding(
+              getSizedBox(10, 0),
+              //! VISIT COMPANY PAGE AND RATING
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  children: [
+                    InkWell(
+                      onTap: () => _launchURL(product!.url),
+                      child: const Text(
+                        'Visit the company store',
+                        style: TextStyle(color: Colors.blue, fontSize: 16),
+                      ),
+                    ),
+                    const Spacer(),
+                    Text(
+                      '${product.rating.toString()}/5.0',
+                      style: const TextStyle(fontSize: 16),
+                    )
+                  ],
+                ),
+              ),
+              //! PRODUCT NAME
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 7),
+                child: Text(
+                  product.name,
+                  style: const TextStyle(
+                      fontSize: 23, fontWeight: FontWeight.w500),
+                ),
+              ),
+              //! RAM STORAGE OPTIONS
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 35,
+                    child: ListView(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        for (var i = 0;
+                            i < product.ramStorageOptions.length;
+                            i++)
+                          returnRamStorageOptions(product, i),
+                      ],
+                    ),
+                  )),
+              //! Price
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                child: Text(
+                  '\$${product.price[0].toString()}',
+                  style: TextStyle(fontSize: 19),
+                ),
+              ),
+              //! ADD TO CART
+              Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 35,
-                  child: ListView(
-                    shrinkWrap: true,
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                      for (var i = 0; i < product.ramStorageOptions.length; i++)
-                        returnRamStorageOptions(product, i),
-                    ],
+                child: InkWell(
+                  onTap: () {},
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: Text("Add to Cart"),
+                    width: double.maxFinite,
+                    height: 40,
+                    decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(8)),
                   ),
-                ))
-          ],
+                ),
+              ),
+              //! BUY NOW
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: InkWell(
+                  onTap: () {},
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: Text("Buy Now"),
+                    width: double.maxFinite,
+                    height: 40,
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 234, 199, 92),
+                        borderRadius: BorderRadius.circular(8)),
+                  ),
+                ),
+              )
+            ],
+          ),
         ));
   }
 
@@ -166,7 +220,6 @@ class _ProductPageState extends State<ProductPage> {
   _launchURL(String url) async {
     Uri _url = Uri.parse(url);
     if (await launchUrl(_url)) {
-      await launchUrl(_url);
     } else {
       throw 'Could not launch $_url';
     }
